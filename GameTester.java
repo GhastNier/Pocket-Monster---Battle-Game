754ebe42fdd307dfdf8fc12f13d40d5e210d157f
SongeDaiDai <74478018+SongeDaiDai@users.noreply.github.com>
2021-12-09 19:44:21 -0500

    Delete GameTester.java

--- GameTester.java
+++
@@ -1,6 +0,0 @@
-public class GameTester {
-    public static void main(String[] args) {
-        int seed = 48;
-        BattleGame.playGame("player.txt", "monster.txt", "spells.txt", seed);
-    }
-}
